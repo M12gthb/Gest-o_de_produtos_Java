9fc27b7869cccb5aa2d5c7154c9cf0b47225516c
MATHEUS BARROS RODRIGUES <mat43214@gmail.com>
2025-03-24 20:47:48 -0300

    feat: product structure

--- src/product/ProductModel.java
+++ src/product/ProductModel.java
@@ -1,5 +1,46 @@
 package product;
 
 public class ProductModel {
+    private String barCode;
+    private String name;
+    private int priceInCents;
+    private int stock;
 
+    public ProductModel(String baCode, String name, int priceInCents, int stock) {
+        this.barCode = baCode;
+        this.name = name;
+        this.priceInCents = priceInCents;
+        this.stock = stock;
+    }
+
+    // Getters
+    public String getBarCode() {
+        return this.barCode;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getPriceInCents() {
+        return this.priceInCents;
+    }
+
+    public int getStock() {
+        return this.stock;
+    }
+
+    // Setters
+
+    public void setName(String newName) {
+        this.name = newName;
+    }
+
+    public void setName(int newPriceInCents) {
+        this.priceInCents = newPriceInCents;
+    }
+
+    public void setStock(int newStock) {
+        this.stock = newStock;
+    }
 }

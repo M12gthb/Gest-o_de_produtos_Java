bdcd0fe086bc010f0014cff3765680d1442e3984
MATHEUS BARROS RODRIGUES <mat43214@gmail.com>
2025-03-26 10:58:15 -0300

    feat: add controller and Model

--- src/product/ProductController.java
+++ src/product/ProductController.java
@@ -1,5 +1,37 @@
 package product;
 
-public class ProductController {
+import java.util.ArrayList;
+import exceptions.NotFoundException;
+import exceptions.NegativePrice;
 
+public class ProductController {
+    private ArrayList<ProductModel> products = new ArrayList<>();
+
+    public String create(ProductModel product) throws NegativePrice {
+        products.add(product);
+        return "Produto " + product.getName() + " adicionado.";
+    }
+
+    public String read() {
+        StringBuilder json = new StringBuilder("[\n");
+
+        for (int i = 0; i < products.size(); i++) {
+            json.append("  ").append(products.get(i).toString());
+            if (i < products.size() - 1) {
+                json.append(",");
+            }
+            json.append("\n");
+        }
+
+        json.append("]");
+        return json.toString();
+    }
+
+    public ProductModel retrieveProduct(String barCode) throws NotFoundException {
+        return products.stream()
+                .filter(p -> p.getBarCode().equals(barCode))
+                .findFirst()
+                .orElseThrow(() -> new NotFoundException(
+                        "Produto com código " + barCode + " não encontrado"));
+    }
 }
--- src/product/ProductModel.java
+++ src/product/ProductModel.java
@@ -1,19 +1,36 @@
 package product;
 
+import exceptions.NegativePrice;
+
 public class ProductModel {
+
+    private static int nextBarCode = 1;
+
     private String barCode;
     private String name;
     private int priceInCents;
     private int stock;
 
-    public ProductModel(String baCode, String name, int priceInCents, int stock) {
-        this.barCode = baCode;
+    @Override
+    public String toString() {
+        return String.format(
+                "{ \"Código\": \"%s\", \"Nome\": \"%s\", \"Preço\": %.2f, \"Estoque\": %d }",
+                barCode,
+                name,
+                priceInCents / 100.0,
+                stock);
+    }
+
+    public ProductModel(String name, int priceInCents, int stock) throws NegativePrice {
+        this.barCode = "PROD-" + nextBarCode++;
+        if (priceInCents < 0) {
+            throw new NegativePrice("O preço do produto precisa ser um número positivo.");
+        }
         this.name = name;
         this.priceInCents = priceInCents;
         this.stock = stock;
     }
 
-    // Getters
     public String getBarCode() {
         return this.barCode;
     }
@@ -29,8 +46,6 @@ public class ProductModel {
     public int getStock() {
         return this.stock;
     }
-
-    // Setters
 
     public void setName(String newName) {
         this.name = newName;

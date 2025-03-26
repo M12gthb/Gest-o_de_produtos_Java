21a62e67ee2f11776fdd3ba956e9ed240b47fb26
MATHEUS BARROS RODRIGUES <mat43214@gmail.com>
2025-03-26 10:57:56 -0300

    feat: add exceptions

--- src/exceptions/NegativePrice.java
+++ src/exceptions/NegativePrice.java
@@ -1,5 +1,12 @@
 package exceptions;
 
-public class NegativePrice {
+public class NegativePrice extends Exception {
+    public NegativePrice() {
+        super("O preço do produto precisa ser um número positivo.");
+    }
+
+    public NegativePrice(String message) {
+        super(message);
+    }
 
 }

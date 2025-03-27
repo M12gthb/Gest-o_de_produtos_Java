bdcf4c066a2a5a4f8152c86697f2726cf2fbec0c
MATHEUS BARROS RODRIGUES <mat43214@gmail.com>
2025-03-27 12:05:59 -0300

    feat: add ProductUpdate

--- src/product/ProductController.java
+++ src/product/ProductController.java
@@ -2,12 +2,17 @@ package product;
 
 import java.util.ArrayList;
 import exceptions.NotFoundException;
+import interfaces.ProductInterfaceUpdate;
 import exceptions.NegativePrice;
+import exceptions.NegativeStock;
 
 public class ProductController {
     private ArrayList<ProductModel> products = new ArrayList<>();
 
-    public String create(ProductModel product) throws NegativePrice {
+    public String create(ProductModel product) throws NegativePrice, NegativeStock {
+        if (product.getStock() < 0) {
+            throw new NegativeStock("O estoque do produto precisa ser um número positivo.");
+        }
         products.add(product);
         return "Produto " + product.getName() + " adicionado.";
     }
@@ -33,5 +38,41 @@ public class ProductController {
                 .findFirst()
                 .orElseThrow(() -> new NotFoundException(
                         "Produto com código " + barCode + " não encontrado"));
+    }
+
+    public String deleteProduct(String barCode) throws NotFoundException {
+        var findProduct = products.stream().filter(p -> p.getBarCode().equals(barCode))
+                .findFirst().orElseThrow(() -> new NotFoundException("Produto com código" + barCode + "encontrado"));
+        products.remove(findProduct);
+        return "Produto deletado";
+    }
+
+    public ProductModel update(ProductInterfaceUpdate product, String barCode)
+            throws NotFoundException, NegativePrice, NegativeStock {
+
+        if (product.price < 0) {
+            throw new NegativePrice("O preço do produto precisa ser um número positivo.");
+        }
+        if (product.stock < 0) {
+            throw new NegativeStock("O estoque do produto precisa ser um número positivo.");
+        }
+        ProductModel productToUpdate = products.stream()
+                .filter(p -> p.getBarCode().equals(barCode))
+                .findFirst()
+                .orElseThrow(() -> new NotFoundException("Produto com código " + barCode + " não encontrado"));
+
+        if (product.name != null && !product.name.isEmpty()) {
+            productToUpdate.setName(product.name);
+        }
+
+        if (product.price > 0) {
+            productToUpdate.setPrice(product.price);
+        }
+
+        if (product.stock > 0) {
+            productToUpdate.setStock(product.stock);
+        }
+
+        return productToUpdate;
     }
 }
